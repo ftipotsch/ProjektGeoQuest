3525c274555df24be24e61793f740cee117e541d
leonidhammer <leonid.h.1999@gmail.com>
2017-05-31 13:00:12 +0200

    install-database.php
    huuups vergessen das fette comment zu discarden

--- Code/install-database.php
+++ Code/install-database.php
@@ -24,32 +24,3 @@ $conn->close();
 
 ?>
 
-//if ($conn->query($sql) === TRUE) {
-echo "Table Users created successfully";
-} else {
-echo "Error creating table: " . $conn->error;
-}
-$conn->close();
-
-// sql to create table
-$sql = "CREATE TABLE Questions (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-question VARCHAR(100) UNIQUE,
-x-coordinate VARCHAR (50),
-y-coordinate VARCHAR (50),
-)";
-
-if ($conn->query($sql) === TRUE) {
-echo "Table Users created successfully";
-} else {
-echo "Error creating table: " . $conn->error;
-}
-$sql = "CREATE TABLE Users (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(30) UNIQUE NOT NULL,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-points INT();
-)";
-
