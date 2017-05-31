298a6841362f2aa218a99d482e47e62ca64a9b93
ftipotsch <ftipotsch@tsn.at>
2017-05-31 13:04:39 +0200

    Verlinkungen

--- Code/delete.php
+++ Code/delete.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kevin Glatz
- * Date: 24.05.2017
- * Time: 13:03
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel ="" href="">
+    <script src="jquery-3.2.1.js"></script>
+</head>
+<body>
+<p>Links</p>
+<p><a href="create.php">Create</a></p>
+<p><a href="read.php">Read</a></p>
+<p><a href="update.php">Update</a></p>
+<p><a href="delete.php">Delete</a></p>
+<p><a href="index.php">Index</a></p>
+
+
+</body>
+</html>
--- Code/index.php
+++ Code/index.php
@@ -20,6 +20,14 @@
     </style>
 </head>
 <body>
+<p>Links</p>
+<p><a href="create.php">Create</a></p>
+<p><a href="read.php">Read</a></p>
+<p><a href="update.php">Update</a></p>
+<p><a href="delete.php">Delete</a></p>
+<p><a href="index.php">Index</a></p>
+
+
 <div id="map"></div>
 <script>
     var map;
