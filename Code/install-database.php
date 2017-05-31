6552ccaf34d1a1995d81d3590a7b1e40a8e33ebb
leonidhammer <leonid.h.1999@gmail.com>
2017-05-31 12:58:39 +0200

    Database
    install-database.php fertig
    install-table.php fertig

--- Code/install-database.php
+++ Code/install-database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -18,21 +18,38 @@ if ($conn->query($sql) === TRUE) {
     echo "Error creating database: " . $conn->error;
 }
 
+
+$conn->close();
+
+
+?>
+
+//if ($conn->query($sql) === TRUE) {
+echo "Table Users created successfully";
+} else {
+echo "Error creating table: " . $conn->error;
+}
+$conn->close();
+
 // sql to create table
-$sql = "CREATE TABLE GeoQuest (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+$sql = "CREATE TABLE Questions (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+question VARCHAR(100) UNIQUE,
+x-coordinate VARCHAR (50),
+y-coordinate VARCHAR (50),
+)";
+
+if ($conn->query($sql) === TRUE) {
+echo "Table Users created successfully";
+} else {
+echo "Error creating table: " . $conn->error;
+}
+$sql = "CREATE TABLE Users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(30) UNIQUE NOT NULL,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
 email VARCHAR(50),
 points INT();
-reg_date TIMESTAMP;
 )";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-$conn->close();
-?>
