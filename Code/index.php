70e38b0cd8ac56b606bad4156a6b709e8199ce68
leonidhammer <leonid.h.1999@gmail.com>
2017-06-12 09:28:17 +0200

    HTML/CSS
    HTML/CSS ein wenig verändert, auf CRUD warten so dass dan mit JS das Pop-Up gemacht werden kann bei dementsprechenden Marker.

--- Code/index.php
+++ Code/index.php
@@ -5,6 +5,7 @@
     <title>Simple Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <script src="Maps.js"></script>
     <style>
         /* Always set the map height explicitly to define the size of the div
@@ -18,16 +19,26 @@
             margin: 0;
             padding: 0;
         }
+
+        
     </style>
 </head>
 <body>
-<p>Links</p>
-<p><a href="create.php">Create</a></p>
-<p><a href="read.php">Read</a></p>
-<p><a href="update.php">Update</a></p>
-<p><a href="delete.php">Delete</a></p>
-<p><a href="index.php">Index</a></p>
 
+<?php
+//LINKS
+?>
+<div id="div">
+    <table id="table" style="width:100%">
+        <tr>
+            <th><a href="create.php">Create</a></th>
+            <th><a href="read.php">Read</a></th>
+            <th><a href="update.php">Update</a></th>
+            <th><a href="delete.php">Delete</a></th>
+            <th><a href="index.php">Index</a></th>
+        </tr>
+    </table>
+</div>
 
 <div id="map" style="height: 400px"></div>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqlwEF-tTeE9tUu94SGdMyOWjRJZk79ZA&callback=initMap"
--- Code/read.php
+++ Code/read.php
@@ -3,16 +3,24 @@
 <head>
     <title></title>
     <link rel ="" href="">
+    <link rel="stylesheet" href="style.css">
     <script src="jquery-3.2.1.js"></script>
 </head>
 <body>
-<p>Links</p>
-<p><a href="create.php">Create</a></p>
-<p><a href="read.php">Read</a></p>
-<p><a href="update.php">Update</a></p>
-<p><a href="delete.php">Delete</a></p>
-<p><a href="index.php">Index</a></p>
-
-
+<?php
+//LINKS
+?>
+<div id="div">
+    <table id="table" style="width:100%">
+        <tr>
+            <th><a href="create.php">Create</a></th>
+            <th><a href="read.php">Read</a></th>
+            <th><a href="update.php">Update</a></th>
+            <th><a href="delete.php">Delete</a></th>
+            <th><a href="index.php">Index</a></th>
+        </tr>
+    </table>
+</div>
+<h1>READ</h1>
 </body>
 </html>
